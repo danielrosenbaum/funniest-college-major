035d02858dbff5d2ec02127a17e6d94013b7aa0a
Daniel Rosenbaum <daniel.rosenbaum10@gmail.com>
2017-11-26 22:23:01 -0800

    Add config vars

---
+++ private.py
@@ -0,0 +1,8 @@
+
+# MySQL configurations
+user = 'b73d493023b378'
+password = 'a68152a3'
+db = 'heroku_21d590c3b6b513a'
+host = 'us-cdbr-iron-east-05.cleardb.net'
+
+# mysql://b73d493023b378:a68152a3@us-cdbr-iron-east-05.cleardb.net/heroku_21d590c3b6b513a?reconnect=true
